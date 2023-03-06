cd51448fea25e4d7a164859c6cf5e0f869a98188
Partab Rai <amrani.partab@gmail.com>
2023-03-06 22:21:59 +0500

    logout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,25 @@
 <script>
   import { request } from "$lib/fetch";
   import { page } from '$app/stores'
+  import { goto } from "$app/navigation";
+
+  let username, email, password;
+
+  const signup = async () => {
+      const userRecord = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+      );
+      await updateProfile(userRecord.user, { displayName: username });
+      const idToken = await getIdToken(userRecord.user, true);
+      await request("/api/auth", "POST", { idToken });
+      goto("/");
+  };
 
   const signOutUser = async () => {
       await request("/api/auth", "DELETE");
-      window.location.replace("/login")
+      goto("/login");
   };
 </script>
 <div>
@@ -15,5 +30,6 @@
    Dont have account dont worry create here
    <a href="/login">Login</a>
    <a href="/register">Register</a>
+   <button on:click={signOutUser}>Logout</button>
   {/if}
 </div>

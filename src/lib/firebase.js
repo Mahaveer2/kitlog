bdffd790611752af3707a785a82c50478621f6a5
Partab Rai <amrani.partab@gmail.com>
2023-03-06 22:02:42 +0500

    this is change

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, setPersistence, inMemoryPersistence } from "firebase/auth"
+
+//TODO: add firebaes config
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ2GxzpdLyqICLXkyKm8lJJsIyq1pzO2w",
+  authDomain: "kitwork-b6c50.firebaseapp.com",
+  projectId: "kitwork-b6c50",
+  storageBucket: "kitwork-b6c50.appspot.com",
+  messagingSenderId: "492731294606",
+  appId: "1:492731294606:web:ef470715a0b77abb1afadf"
+};
+
+const app = initializeApp(firebaseConfig, "CLIENT");
+export const auth = getAuth(app)
+
+setPersistence(auth, inMemoryPersistence)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { request } from "$lib/fetch";
+  import { page } from '$app/stores'
+
+  const signOutUser = async () => {
+      await request("/api/auth", "DELETE");
+      window.location.replace("/login")
+  };
+</script>
+<div>
+  {#if $page.data.user}
+   <p>Username : {$page.data.user.displayName}</p>
+   <p>Email : {$page.data.user.email}</p>
+   {:else}
+   Dont have account dont worry create here
+   <a href="/login">Login</a>
+   <a href="/register">Register</a>
+  {/if}
+</div>
